62a7a8136a2f2a7fd258ac421ed0afc8ec76e363
Pablovm1990 <pablovm1990@gmail.com>
2015-01-30 18:03:22 +0100

    pipeline in comments

--- source/main.R
+++ source/main.R
@@ -1,5 +1,9 @@
 main <- function (nGenes = 1000, nSubjects = 50, kFold = 10, selectedGenes = 10, nTimes = 10) {
   "hola hermosos"
   attach(createDataset(nGenes, nSubjects))
+  #Divide in testing and training genes
   
+  #Filter with the p-value the training set
+  
+  #Execute randomForest
 }
