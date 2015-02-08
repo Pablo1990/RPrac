6e94e52ac5e6af2f9cf167bcd6455cba19f3ad85
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-02-08 16:27:58 +0100

    now is ok the createDataset

--- source/createDataset.R
+++ source/createDataset.R
@@ -1,13 +1,16 @@
 #Create dataset, but depending on the arguments: 
 # number of genes, number of subjects
-createDataset <- function(nGenes = 1000, nSubjects = 50){
+createDataset <- function(nGenes = 100, nSubjects = 50){
   #￼help(rnorm) help(runif) help(rpois)
   #set.seed(2) #for testing
+  if(nSubjects%2==1){
+    
+  }
   type <- factor(c(rep("NONAFFECTED", nSubjects/2), rep("AFFECTED", nSubjects/2)))
   
-  genes <- matrix(rnorm(nSubjects * nGenes), ncol = nSubjects) #page 69 R-Bioinfo-intro 
+  genes <- matrix(rnorm(nGenes*nSubjects), nrow = nSubjects) #page 69 R-Bioinfo-intro
   
-  pvalues <- apply(genes, 1,
+  pvalues <- apply(genes, 2,
                    function(x) t.test(x ~ type)$p.value)
   #(hist(pvalues))
   #pvalues
