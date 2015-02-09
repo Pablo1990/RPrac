77b3d2ef603d336bf37d8c5bfaa0df813dd64abb
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-02-09 13:36:48 +0100

    trying to see what can we visualize

--- source/visualizeData.R
+++ source/visualizeData.R
@@ -1,3 +1,12 @@
 visualizeData <- function(){
-  
+  #hist, plot, scatter3d (library(car)), bloxplot, stripchart, scatterplot
+  #Además del error de clasificación, usad los votos o probabilidades que
+  #da randomForest cuando predice algo, para obtener el score de Brier y/o
+  #el índice de concordancia.
+  library("ModelGood")
+  library("ROCR")
+  #Brier()
+  pred <- prediction(predictions, labels)
+  perf <- performance(pred, measure = "tpr", x.measure = "fpr")
+  plot(perf, col=rainbow(10))
 }
