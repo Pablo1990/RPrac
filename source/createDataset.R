6bec6eecdb5b31de479522e14f2f2395707f0015
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-02-09 13:45:20 +0100

    changes from joseale to createDataset. All integrated now.

--- source/createDataset.R
+++ source/createDataset.R
@@ -4,14 +4,18 @@ createDataset <- function(nGenes = 100, nSubjects = 50){
   #￼help(rnorm) help(runif) help(rpois)
   #set.seed(2) #for testing
   
-  type <- factor(rep(c("NONAFFECTED","AFFECTED"),length.out = nSubjects))
+  #type <- factor(rep(c("NONAFFECTED","AFFECTED"),length.out = nSubjects))
+  #genes <- matrix(rnorm(nGenes*nSubjects), nrow = nSubjects) #page 69 R-Bioinfo-intro
   
-  genes <- matrix(rnorm(nGenes*nSubjects), nrow = nSubjects) #page 69 R-Bioinfo-intro
+  data <- data.frame (gene = matrix(rnorm(nGenes*nSubjects), nrow = nSubjects), #page 69 R-Bioinfo-intro
+                      type = factor(rep(c("NONAFFECTED","AFFECTED"),length.out = nSubjects)))
   
-  pvalues <- apply(genes, 2,
-                   function(x) t.test(x ~ type)$p.value)
+  #pvalues <- apply(genes, 2,
+  #                 function(x) t.test(x ~ type)$p.value)
+  pvalues <- apply(data[1:nrow(data),1:ncol(data)-1], 2, 
+                   function(x) t.test(x ~ data$type)$p.value)
   #(hist(pvalues))
   #pvalues
-  return (list(type = type, genes = genes, pvalues = pvalues))
+  return (list(data = data, pvalues = pvalues))
   #attach(createDataset(1000, 50))
 }
