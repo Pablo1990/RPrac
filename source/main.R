f85e7fe2302b27ce3280a036ca43c73d6c6ccfb2
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-02-11 13:04:54 +0100

    YEEES!!! IT WORKS!!!

--- source/main.R
+++ source/main.R
@@ -5,21 +5,24 @@ main <- function (nGenes = 100, nSubjects = 50, kFold = 10, selectedGenes = 10, 
   library('randomForest')
   library("ROCR")
   datos <- createDataset(nGenes, nSubjects)
-  datos$data <- filter(data, selectedGenes, nGenes)
+  #datos$data <- filter(data, selectedGenes, nGenes)
+  #print(datos$pvalues)
   cont <- 0
   while(cont < nTimes) {
     #Divide in testing and training genes
 
     index.select <- kfolding(datos,kFold)
     for(sample.number in 1:kFold) {
-      datos$data.train <- datos$data[,index.select != sample.number]
-      datos$data.test <- datos$data[,index.select == sample.number]
+      datos$data.train <- datos$data[index.select != sample.number,]
+      datos$data.test <- datos$data[index.select == sample.number,]
+      datos$type.train <- datos$type[index.select != sample.number]
+      datos$type.test <- datos$type[index.select == sample.number]
       #print(datos$data.test)
       #print("-------------------------------")
       #print(datos$data.train)
       #model <- randomForest(genes, type, mtry=2, ntree=1000, keep.forest=TRUE, importance=TRUE)
-      myrf <- randomForest(y=datos$type, x=datos$data.train, mtry=2, ntree=500, keep.forest=TRUE, importance=TRUE)
-      mypredict <- predict(myrf, datos$data)
+      myrf <- randomForest(y=datos$type.train, x=datos$data.train,mtry=2, ntree=500, keep.forest=TRUE, importance=TRUE)
+      mypredict <- predict(myrf, datos$data.test)
       
       #(rfcv(datos$data.test, datos$type, cv.fold=kFold)) #esto es algo que hace crossvalidation
       #pero no entiendo nada de lo que hace
@@ -30,7 +33,7 @@ main <- function (nGenes = 100, nSubjects = 50, kFold = 10, selectedGenes = 10, 
       #print(myrf)
       #print(myrf$predicted)
       #print(myrf$confusion)
-      break
+      
     }
     
     cont <- cont + 1
