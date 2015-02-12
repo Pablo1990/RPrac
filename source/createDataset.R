5080fb616690060c3a57c632bfc69c33d0874cc8
Pablovm1990 <pablovm1990@gmail.com>
2015-02-12 17:41:42 +0100

    documented createDataset

--- source/createDataset.R
+++ source/createDataset.R
@@ -1,22 +1,21 @@
 #Create dataset, but depending on the arguments: 
-# number of genes, number of subjects
+#nGenes: number of genes involved in the study
+#nSubjects: number of subjects involved in the study
 createDataset <- function(nGenes = 100, nSubjects = 50){
   #￼help(rnorm) help(runif) help(rpois)
   #set.seed(2) #for testing
   
-  #type <- factor(rep(c("NONAFFECTED","AFFECTED"),length.out = nSubjects))
-  #genes <- matrix(rnorm(nGenes*nSubjects), nrow = nSubjects) #page 69 R-Bioinfo-intro
+  #We create a data frame with genes in columns, and in rows the subjects.
+  data <- data.frame (gene = matrix(rnorm(nGenes*nSubjects), nrow = nSubjects))
   
-  data <- data.frame (gene = matrix(rnorm(nGenes*nSubjects), nrow = nSubjects)) #page 69 R-Bioinfo-intro
-  
+  #Generating the types of the subjects divide in affected and nonAffected
   type <- factor(rep(c("NONAFFECTED","AFFECTED"),length.out = nSubjects))
   
-  #pvalues <- apply(genes, 2,
-  #                 function(x) t.test(x ~ type)$p.value)
+  #Calculate the pvalues of every gene
   pvalues <- apply(data[1:nrow(data),1:ncol(data)], 2, 
                    function(x) t.test(x ~ type)$p.value)
   #(hist(pvalues))
-  #pvalues
+  
+  #Return every variable calculated
   return (list(data = data, pvalues = pvalues, type = type))
-  #attach(createDataset(1000, 50))
 }
