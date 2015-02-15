2237745e47a389d5dd7bfd777e1c121fd85a5c96
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-02-15 16:13:29 +0100

    commented

--- source/filterPvalueBad.R
+++ source/filterPvalueBad.R
@@ -1,20 +1,21 @@
-#Feature selection
+#Feature selection. It select most important genes according to the pvalue
 #Arguments:
-#datos: the whole dataset
+#data: the whole dataset
 #selectedGenes: number of genes we are going to select for training dataset
 #nGenes: number of genes involved in the study
 filter <- function (datos,selectedGenes,nGenes) {
-  
+  #Get the N selectedGenes of the training set according to the lowest pvalues.
   filtered <- datos$pvalues <= quantile (datos$pvalues,(selectedGenes/nGenes))
   
+  #Do the filtered to the genes
   positions <- which(!filtered)
-  
   data <- datos$data[,-positions]
   
+  #And the types too
   type <- datos$type
-  
   pvalues <- datos$pvalues[-positions]
   
+  #Return the filtered data
   return (list(data = data, pvalues = pvalues, type = type))
 }
 
--- source/filterPvalueGood.R
+++ source/filterPvalueGood.R
@@ -1,18 +1,20 @@
-#Feature selection. It select 
+#Feature selection. It select most important genes according to the pvalue
 #Arguments:
 #data: the whole dataset
 #selectedGenes: number of genes we are going to select for training dataset
 #nGenes: number of genes involved in the study
 filter <- function (data,selectedGenes,nGenes) {
-  
+  #Get the pvalues of the training set
   train.pvalues <- apply(data$data.train[1:nrow(data$data.train),1:ncol(data$data.train)], 2, 
                    function(x) t.test(x ~ data$type.train)$p.value)
   
+  #Get the N selectedGenes of the training set according to the lowest pvalues.
   filtered <- train.pvalues <= quantile (train.pvalues,(selectedGenes/nGenes))
   
+  #Do the filtered
   positions <- which(!filtered)
-  
   datafiltered <- data$data.train[,-positions]
   
+  #Return the train filtered
   return (datafiltered)
 }
