2ffe6fdd1b41e8beab9e9c3a800be0d2e98e187a
joseale2310 <joseale2310@hotmail.es>
2015-02-18 12:39:44 +0100

    refining for the package

--- source/filterPvalueBad.R
+++ source/filterPvalueBad.R
@@ -1,20 +1,20 @@
-#Feature selection. It select most important genes according to the pvalue
+#Feature selection. It selects the most important genes according to the pvalue
 #Arguments:
 #data: the whole dataset
 #selectedGenes: number of genes we are going to select for training dataset
 #nGenes: number of genes involved in the study
 #return the filtered data
-filterBad <- function (datos,selectedGenes,nGenes) {
+filterBad <- function (x,selectedGenes,nGenes) {
   #Get the N selectedGenes of the training set according to the lowest pvalues.
-  filtered <- datos$pvalues <= quantile (datos$pvalues,(selectedGenes/nGenes))
+  filtered <- x$pvalues <= quantile (datos$pvalues,(selectedGenes/nGenes))
   
   #Do the filtered to the genes
   positions <- which(!filtered)
-  data <- datos$data[,-positions]
+  data <- x$data[,-positions]
   
   #And the types too
-  type <- datos$type
-  pvalues <- datos$pvalues[-positions]
+  type <- x$type
+  pvalues <- x$pvalues[-positions]
   
   #Return the filtered data
   return (list(data = data, pvalues = pvalues, type = type))
